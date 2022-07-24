648fb40a9989b1410942e5a9926621bd4f22cd3d
Greg <gskril@gmail.com>
2022-07-24 03:08:59 -0400

    Connect MongoDB and set <head> content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,34 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Farcaster Search | Searchcaster</title>
+				<link rel="apple-touch-icon" href="/img/search-logo.png" />
+				<link rel="icon" href="/img/favicon-16.png" sizes="16x16" />
+				<link rel="icon" href="/img/favicon-32.png" sizes="32x32" />
+				<link rel="icon" href="/img/favicon-64.png" sizes="64x64" />
+				<link rel="icon" href="/img/favicon-96.png" sizes="96x96" />
+
+				<meta
+					property="og:image"
+					content="https://searchcaster.xyz/img/opengraph.png"
+				/>
+				<meta
+					property="og:description"
+					name="description"
+					content="Search for any cast"
+				/>
+				<meta
+					name="description"
+					content="Search for any cast on the Farcaster protocol."
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default App
