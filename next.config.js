d9efc6b08508f0ec61eb265a5e3444b5f59b9d7e
Greg <gskril@gmail.com>
2022-07-25 00:26:04 -0400

    Add /api/ens endpoint and API docs page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+	async redirects() {
+		return [
+			{
+				source: '/api',
+				destination: '/docs',
+				permanent: false,
+			},
+		]
+	},
+	reactStrictMode: true,
+	swcMinify: true,
 }
-
-module.exports = nextConfig
---
+++ src/pages/api/ens.js
@@ -0,0 +1,38 @@
+import clientPromise from '../../lib/db'
+
+export default async function handler(req, res) {
+	const client = await clientPromise
+	const db = client.db('farcaster')
+	const collection = db.collection('casts')
+
+	const { parent } = req.query
+	let casts = []
+
+	if (parent) {
+		casts = collection.find({
+			$and: [
+				{ 'body.data.text': { $regex: /[^\s]\.+eth/ } },
+				{ 'body.data.replyParentMerkleRoot': parent },
+			],
+		})
+	} else {
+		return res.status(400).json({
+			error: 'Missing parent',
+		})
+	}
+
+	const json = await casts.sort({ 'body.publishedAt': -1 }).toArray()
+
+	return res.json(
+		json.map((cast) => {
+			return {
+				text: cast.body.data.text,
+				username: cast.body.username,
+				displayName: cast.meta.displayName || null,
+				merkleRoot: cast.merkleRoot,
+				replyParentMerkleRoot:
+					cast.body.data.replyParentMerkleRoot || null,
+			}
+		})
+	)
+}
