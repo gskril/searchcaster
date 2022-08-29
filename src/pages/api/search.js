775447f138eefac45102d303fb3b1bb7427741e9
Greg <gskril@gmail.com>
2022-08-28 22:16:54 -0400

    Switch from mongodb to supabase

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,11 +1,8 @@
-import clientPromise from '../../lib/db'
+import supabase from '../../lib/db'
 import { formatCasts } from '../../utils/cast'
 
 export async function searchCasts(query) {
   const startTime = Date.now()
-  const client = await clientPromise
-  const db = client.db('farcaster')
-  const collection = db.collection('casts')
 
   let {
     after,
@@ -24,148 +21,104 @@ export async function searchCasts(query) {
   count = Math.min(parseInt(count), 50) || 25
   page = parseInt(page) || 1
   const offset = (page - 1) * count
+  const upperRange = offset + count - 1
   const textQuery = text ? text.toString() : ''
   after = Number(after) || 0
   before = Number(before) || new Date().getTime()
 
-  if (merkleRoot) {
-    casts = collection.find({
-      $or: [
-        { merkleRoot: merkleRoot },
-        { 'body.data.replyParentMerkleRoot': merkleRoot },
-      ],
-    })
-  } else if (media) {
+  if (media) {
     if (media === 'image') {
-      casts = collection.find({
-        'body.data.text': { $regex: /i.imgur.com/ },
-      })
+      casts = await supabase
+        .from('casts')
+        .select()
+        .ilike('text', '%https://i.imgur.com/%')
+        .gt('published_at', after)
+        .lt('published_at', before)
+        .range(offset, upperRange)
+        .order(
+          engagement
+            ? engagement === 'reactions'
+              ? 'reaction_count'
+              : engagement
+            : 'published_at',
+          { ascending: false }
+        )
     } else if (media === 'music') {
-      casts = collection.find({
-        'body.data.text': {
-          $regex: /(open.spotify.com|soundcloud.com|music.apple.com|tidal.com)/,
-        },
-      })
+      casts = await supabase
+        .from('casts')
+        .select()
+        .or(
+          'text.ilike.%open.spotify.com%,text.ilike.%soundcloud.com%,text.ilike.%music.apple.com%,text.ilike.%tidal.com%'
+        )
+        .gt('published_at', after)
+        .lt('published_at', before)
+        .range(offset, upperRange)
+        .order(
+          engagement
+            ? engagement === 'reactions'
+              ? 'reaction_count'
+              : engagement
+            : 'published_at',
+          { ascending: false }
+        )
     } else if (media === 'youtube') {
-      casts = collection.find({
-        'body.data.text': {
-          $regex: /(youtube.com|youtu.be)/,
-        },
-      })
+      casts = await supabase
+        .from('casts')
+        .select()
+        .or('text.ilike.%youtube.com%,text.ilike.%youtu.be%')
+        .gt('published_at', after)
+        .lt('published_at', before)
+        .range(offset, upperRange)
+        .order(
+          engagement
+            ? engagement === 'reactions'
+              ? 'reaction_count'
+              : engagement
+            : 'published_at',
+          { ascending: false }
+        )
     } else if (media === 'url') {
-      casts = collection.find({
-        $and: [
-          { 'body.data.text': { $regex: /(http:\/\/|https:\/\/)/ } },
-          { 'body.data.text': { $regex: /(.com|.xyz)/ } },
-          { 'body.data.text': { $not: /i.imgur.com/ } },
-        ],
-      })
+      casts = await supabase
+        .from('casts')
+        .select()
+        .or('text.ilike.%http://%,text.ilike.%https://%')
+        .not('text', 'ilike', '%https://i.imgur.com/%')
+        .gt('published_at', after)
+        .lt('published_at', before)
+        .range(offset, upperRange)
+        .order(
+          engagement
+            ? engagement === 'reactions'
+              ? 'reaction_count'
+              : engagement
+            : 'published_at',
+          { ascending: false }
+        )
     }
-  } else if (username) {
-    if (text) {
-      casts = collection.find({
-        $and: [
-          { 'body.username': username.toLowerCase() },
-          {
-            'body.data.text': {
-              $regex: textQuery,
-              $options: 'i',
-            },
-          },
-          {
-            'body.data.text': {
-              $not: {
-                $regex: '^(delete:farcaster://|recast:farcaster://)',
-              },
-            },
-          },
-          // make sure that cast.body.publishedAt is after 'after' and before 'before'
-          {
-            'body.publishedAt': {
-              $gte: after,
-              $lte: before,
-            },
-          },
-        ],
-      })
-    } else {
-      casts = collection.find({
-        $and: [
-          { 'body.username': username.toLowerCase() },
-          {
-            'body.data.text': {
-              $not: {
-                $regex: '^(delete:farcaster://|recast:farcaster://)',
-              },
-            },
-          },
-          // make sure that cast.body.publishedAt is after 'after' and before 'before'
-          {
-            'body.publishedAt': {
-              $gte: after,
-              $lte: before,
-            },
-          },
-        ],
-      })
-    }
-  } else if (regex) {
-    casts = collection.find({
-      // regex for a string that starts with gm or gn
-      $and: [
-        {
-          'body.data.text': {
-            $regex: regex,
-          },
-        },
-        {
-          'body.data.text': {
-            $not: {
-              $regex: '^(delete:farcaster://|recast:farcaster://)',
-            },
-          },
-        },
-      ]
-    })
   } else {
-    casts = collection.find({
-      $and: [
-        {
-          'body.data.text': {
-            $regex: textQuery,
-            $options: 'i',
-          },
-        },
-        {
-          'body.data.text': {
-            $not: {
-              $regex: '^(delete:farcaster://|recast:farcaster://)',
-            },
-          },
-        },
-        // make sure that cast.body.publishedAt is after 'after' and before 'before'
-        {
-          'body.publishedAt': {
-            $gt: after,
-            $lt: before,
-          },
-        },
-      ],
-    })
+    casts = await supabase
+      .from('casts')
+      .select()
+      .ilike('merkle_root', merkleRoot ? merkleRoot : '%')
+      .ilike('username', username ? username : '%')
+      .ilike('text', textQuery ? `%${textQuery}%` : '%')
+      .gt('published_at', after)
+      .lt('published_at', before)
+      .range(offset, upperRange)
+      .order(
+        engagement
+          ? engagement === 'reactions'
+            ? 'reaction_count'
+            : engagement
+          : 'published_at',
+        { ascending: false }
+      )
   }
 
-  const filterMethod = engagement
-    ? `meta.${engagement}.count`
-    : 'body.publishedAt'
-
-  const response = await casts
-    .sort({ [filterMethod]: -1 })
-    .limit(count)
-    .skip(offset)
-    .toArray()
+  // TODO: support regex search param
 
   // Restructure data
-  const formattedResponse = formatCasts(response)
+  const formattedResponse = formatCasts(casts.data)
 
   const endTime = Date.now()
   const elapsedTime = endTime - startTime
