cf331382fe0d76498dd9170920bc9739b60999fa
Greg <gskril@gmail.com>
2022-09-03 10:14:18 -0400

    Revert "Temporarily remove wallet gating"
    This reverts commit 61088f94e3b3015fee14bc8c70d6090025b81295.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { usePlausible } from 'next-plausible'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 
 import Suggestion from '../components/suggestion'
 
@@ -41,6 +41,12 @@ export default function Home() {
         <form
           onSubmit={(e) => {
             e.preventDefault()
+            if (!hasEthereum) {
+              // Plausible Analytics
+              plausible('Denied')
+
+              return toast.error('You need an Ethereum wallet :)')
+            }
             const query = e.target.text.value
 
             // Plausible Analytics
