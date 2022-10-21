c3dfe862be35ade7aa18c68f694d9ce5bb176350
Greg <gskril@gmail.com>
2022-10-20 23:46:56 -0400

    Fix logo sizes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 export default function Logo({ align, size, ...props }) {
-  const imageSize = size === 'lg' ? 42 : 32
+  const imageSize = size === 'lg' ? 42 : 26
 
   return (
     <>
@@ -27,21 +27,22 @@ export default function Logo({ align, size, ...props }) {
         .logo {
           line-height: 0;
           margin-right: ${size === 'lg' ? '0.75rem' : '0.5rem'};
-          width: ${imageSize};
-          height: ${imageSize};
+          width: ${imageSize + 'px'};
+          height: ${imageSize + 'px'};
 
           @media screen and (max-width: 30em) {
-            width: 32px;
-            height: 32px;
+            margin-right: ${size === 'lg' ? '0.5rem' : '0.5rem'};
+            width: ${size === 'lg' ? '32px' : '26px'};
+            height: ${size === 'lg' ? '32px' : '26px'};
           }
         }
 
         h1 {
-          font-size: ${size === 'lg' ? '2.5rem' : '2rem'};
+          font-size: ${size === 'lg' ? '2.5rem' : '1.5rem'};
           color: #fff;
 
           @media screen and (max-width: 30em) {
-            font-size: 1.8rem;
+            font-size: ${size === 'lg' ? '1.8rem' : '1.5rem'};
           }
         }
       `}</style>
