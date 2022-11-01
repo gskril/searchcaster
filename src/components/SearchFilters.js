f7f4993168981b44491d274a9f869b6e64e8d2b8
Greg <gskril@gmail.com>
2022-11-01 23:05:16 +0100

    Improve Sorting UI on mobile

--- src/components/SearchFilters.js
+++ src/components/SearchFilters.js
@@ -5,53 +5,88 @@ export default function SearchFilters({ query }) {
 
   return (
     <>
-      <div className="filters_wrapper">
-        {filters.map((filter) => {
-          const stringifiedQuery = JSON.stringify(query).slice(1, -1)
-          const stringifiedQueryChanges = JSON.stringify(
-            filter.queryChanges
-          ).slice(1, -1)
+      <div className="filters">
+        <span className="filters_label">Sort:</span>
+        <div className="filters_wrapper">
+          {filters.map((filter) => {
+            const stringifiedQuery = JSON.stringify(query).slice(1, -1)
+            const stringifiedQueryChanges = JSON.stringify(
+              filter.queryChanges
+            ).slice(1, -1)
 
-          const isActive = stringifiedQuery.includes(stringifiedQueryChanges)
+            const isActive = stringifiedQuery.includes(stringifiedQueryChanges)
 
-          return (
-            <button
-              key={filter.label}
-              className={`filter ${isActive ? 'filter--active' : ''}`}
-              onClick={() => {
-                router.push({
-                  pathname: '/search',
-                  query: {
-                    ...query,
-                    ...filter.queryChanges,
-                    page: 1,
-                  },
-                })
-              }}
-            >
-              {filter.label}
-            </button>
-          )
-        })}
+            return (
+              <button
+                key={filter.label}
+                className={`filter ${isActive ? 'filter--active' : ''}`}
+                onClick={() => {
+                  router.push({
+                    pathname: '/search',
+                    query: {
+                      ...query,
+                      ...filter.queryChanges,
+                      page: 1,
+                    },
+                  })
+                }}
+              >
+                {filter.label}
+              </button>
+            )
+          })}
+        </div>
       </div>
 
       <style jsx>{`
+        .filters {
+          display: flex;
+          gap: 2rem;
+          align-items: center;
+          justify-content: space-between;
+          position: relative;
+
+          &::after {
+            content: '';
+            position: absolute;
+            pointer-events: none;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            width: 4rem;
+            height: calc(100% - 0.75rem);
+            background: linear-gradient(
+              to right,
+              rgba(28, 22, 38, 0),
+              rgb(28, 22, 38)
+            );
+          }
+        }
+
+        .filters_label {
+          padding-bottom: 0.75rem;
+          font-weight: 500;
+
+          @media (min-width: 500px) {
+            display: none;
+          }
+        }
+
         .filters_wrapper {
           display: flex;
-          flex-wrap: wrap;
-          margin-bottom: 0.75rem;
-          gap: 0.75rem;
-          list-style: none;
-          padding: 0;
+          gap: 0.5rem;
+          overflow: scroll;
+          padding-bottom: 0.75rem;
         }
 
         .filter {
           margin: 0;
-          width: fit-content;
-          padding: 0.125rem 0.75rem;
+          width: max-content;
+          min-width: max-content;
+          padding: 0.125rem 1rem;
           color: #fff;
           border: 1px solid var(--primary-color-hover);
-          border-radius: 0.25rem;
+          border-radius: 1rem;
           transition: all 0.2s ease-in-out;
 
           &--active {
