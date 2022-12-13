0883b513733b29c57c0f1b421cdbb6b17b51006b
Greg <gskril@gmail.com>
2022-12-12 22:04:37 -0500

    Update cast search endpoint

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-)
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY
+
+if (!SUPABASE_URL || !SUPABASE_SERVICE_KEY) {
+  throw new Error('Missing Supabase URL or service key')
+}
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
 export default supabase
