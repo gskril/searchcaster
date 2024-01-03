03fb485d8dbe52a2248d3353dfc2f15164caa949
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-01-03 17:31:51 -0500

    Allow searching for casts by domain in the embed

--- src/pages/api/embeds.ts
+++ src/pages/api/embeds.ts
@@ -4,8 +4,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import supabase from '../../lib/db'
 import { formatCasts } from '../../utils/cast'
 
+// TODO: add pagination to this endpoint
 const schema = z.object({
-  url: z.string().url(),
+  url: z.string().url().optional(),
+  domain: z.string().optional(),
 })
 
 type Response = {
@@ -23,19 +25,39 @@ export default async function handler(
     res.status(400).json({ error: safeParse.error })
     return
   }
+  let unformattedCasts = new Array()
+  const { domain, url } = schema.parse(req.query)
 
-  const { url } = schema.parse(req.query)
+  if (domain) {
+    const { data, error } = await supabase
+      .rpc('find_casts_by_domain', {
+        domain,
+      })
+      .order('published_at', { ascending: false })
+      .limit(100)
 
-  const { data, error } = await supabase.rpc('find_casts_by_url', {
-    dynamic_url: url,
-  })
+    if (error) {
+      res.status(500).json({ error })
+      return
+    }
 
-  const casts = formatCasts(data)
+    unformattedCasts = data
+  } else if (url) {
+    const { data, error } = await supabase
+      .rpc('find_casts_by_url', {
+        dynamic_url: url,
+      })
+      .order('published_at', { ascending: false })
+      .limit(100)
 
-  if (error) {
-    res.status(500).json({ error })
-    return
+    if (error) {
+      res.status(500).json({ error })
+      return
+    }
+
+    unformattedCasts = data
   }
 
+  const casts = formatCasts(unformattedCasts)
   return res.status(200).json({ result: casts })
 }
